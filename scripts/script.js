25e472f20d099bdfba938cb410f9bb3db947d9e7
FromDota1 <alik_99999@mail.ru>
2024-05-23 04:54:11 +0500

    [ADD] add close list sortable

--- scripts/script.js
+++ scripts/script.js
@@ -12,8 +12,7 @@ function Item(title, subitems) {
 }
 
 function ViewModel() {
-  var self = this;
-  self.isDragging = ko.observable(false);
+  let self = this;
   self.items = ko.observableArray([
     new Item("Обязательные для всех", [
       new SubItem("Паспорт"),
@@ -32,5 +31,52 @@ function ViewModel() {
     ]),
   ]);
   self.items()[0].isOpen(true);
+  self.addChild = function (subitem, targetArray) {
+    // console.log("targetArray", targetArray,subitem);
+    targetArray.push(subitem);
+  };
+  self.checkCondition = function (targetItem, subitem) {
+    return targetItem.title() !== "Специальные";
+  };
+
+  self.receiveSubitem = function (event, ui) {
+    var item = ko.dataFor(ui.item[0]);
+    if (item && item.title) {
+      $(ui.sender).sortable("cancel");
+    }
+  };
+  self.receiveMain = function (event, ui) {
+    let subitem = ko.dataFor(ui.item[0]);
+    if (subitem && subitem.name) {
+      let targetIndex = $(ui.item).prev().attr("data-index");
+      let sourceItem = ko.dataFor(ui.sender[0]);
+      targetItem = this.items().filter((e, i) => targetIndex == i)[0];
+      if (targetItem && targetItem.subitems) {
+        sourceItem.subitems.remove(subitem);
+        targetItem.subitems.push(subitem);
+        $(ui.sender).sortable("cancel");
+        setTimeout(() => {
+          $(ui.item).remove();
+        }, 0);
+      }
+    }
+  };
 }
+
 ko.applyBindings(new ViewModel());
+
+$(function () {
+  $(".draggable__list__item__sublist").sortable({
+    connectWith: ".draggable__list__item__sublist, .draggable__list",
+    receive: function (event, ui) {
+      ko.contextFor(this).$root.receiveSubitem(event, ui);
+    },
+  });
+  $(".draggable__list").sortable({
+    connectWith: ".draggable__list__item__sublist",
+
+    update: function (event, ui) {
+      ko.contextFor(this).$root.receiveMain(event, ui);
+    },
+  });
+});
